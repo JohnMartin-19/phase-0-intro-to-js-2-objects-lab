9ca4395568f1dd99d4e037975d3ed901bf22a8c5
John Mburu <johnnkonge2020@gmail.com>
2023-09-13 11:20:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employees = {
+    'john':'1234',
+};
+employees;
+
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    const newObj = {...obj};
+    newObj[key]= value;
+    console.log(newObj);
+}
+updateEmployeeWithKeyAndValue(employees,'Peter','1256');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key]=value;
+    console.log(obj); 
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employees,'mark','naks');
+
+function deleteFromEmployeeByKey(){
+    delete employees.john;
+    console.log(employees)
+}
+deleteFromEmployeeByKey();
+
+console.log(employees);
